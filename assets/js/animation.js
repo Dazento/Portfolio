18658a403d074e3bcde9b9d45068c2886675a1cc
DylanB <dylanbury.pro@gmail.com>
2023-01-24 18:03:00 +0100

    gsap animation added for hero, intro and project sections

---
+++ assets/js/animation.js
@@ -0,0 +1,39 @@
+gsap.registerPlugin(ScrollTrigger);
+let tl = gsap.timeline({ ease: "power4.inOut" });
+
+tl.to('.navbar__links li', { stagger: .2, y: 0, opacity: 1, duration: .5 })
+tl.to('.hero__intro *', { 'clip-path': 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)', stagger: .2, y: 0, opacity: 1, duration: 1 }, '-=1');
+tl.to('.hero__scroll', { y: 0, opacity: 1, duration: .8 }, '-=1.2')
+
+gsap.to('.intro div', {
+  scrollTrigger: {
+    trigger: '.intro',
+    end: 'top',
+    scrub: 1
+  },
+  stagger: .2,
+  x: 0,
+  opacity: 1
+});
+
+gsap.to('.projects__project--right', {
+  scrollTrigger: {
+    trigger: '.projects__project--right',
+    end: 'top',
+    scrub: 1
+  },
+  x: 0,
+  opacity: 1,
+  duration: 1
+});
+
+gsap.to('.projects__project--left', {
+  scrollTrigger: {
+    trigger: '.projects__project--left',
+    end: 'top',
+    scrub: 1
+  },
+  x: 0,
+  opacity: 1,
+  duration: 1
+});
